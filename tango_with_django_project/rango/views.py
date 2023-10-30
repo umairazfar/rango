c6d47a69176c13bf15a00c1a155336178ef33eb6
Umair Azfar Khan <umairazfar@gmail.com>
2023-10-30 09:03:04 +0000

    2 tests are failing

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says Hi")
+    return HttpResponse("<a href='/rango/about/'>About</a> Rango says hey there partner!")
+
+def about(request):
+    return HttpResponse("<a href='/rango/'>Index</a> Rango says here is the about page.")
